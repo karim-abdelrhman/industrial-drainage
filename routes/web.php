2f87ff10bd7872b3cd0b4866db243671c1949b49
Karim Abdelrhman <kabdo2536@gmail.com>
2026-09-21 20:05:50 +0300

    Redirect root URL to admin dashboard; remove welcome view route for streamlined navigation.

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\InvoicePrintController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
 
 Route::middleware('auth:admin')->prefix('invoices')->name('invoices.')->group(function () {
     Route::get('{invoice}/print', [InvoicePrintController::class, 'html'])->name('print');
